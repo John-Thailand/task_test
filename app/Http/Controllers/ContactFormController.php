cbf006e1e7e021eecd414dd53270203086c5d837
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-17 21:17:26 +0900

    deleteの実装

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -135,6 +135,9 @@ class ContactFormController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $contact = ContactForm::find($id);
+        $contact->delete();
+
+        return to_route('contacts.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::prefix('contacts')
         Route::get('/{id}', 'show')->name('show');
         Route::get('/{id}/edit', 'edit')->name('edit');
         Route::post('/{id}', 'update')->name('update');
+        Route::post('/{id}/destroy', 'destroy')->name('destroy');
     });
 // Route::get('contacts', [ContactFormController::class, 'index'])->name('contacts.index');
 // Route::get('contacts', [ContactFormController::class, 'index'])->name('contacts.index');
