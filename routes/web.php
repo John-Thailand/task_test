b8be2816696229f5aeea45ff042eae33b50479c0
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-17 21:10:49 +0900

    update 更新画面

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::prefix('contacts')
         Route::post('/', 'store')->name('store');
         Route::get('/{id}', 'show')->name('show');
         Route::get('/{id}/edit', 'edit')->name('edit');
+        Route::post('/{id}', 'update')->name('update');
     });
 // Route::get('contacts', [ContactFormController::class, 'index'])->name('contacts.index');
 // Route::get('contacts', [ContactFormController::class, 'index'])->name('contacts.index');
